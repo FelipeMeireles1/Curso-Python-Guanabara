becd50b15a5db37e7387b0132624826321089641
Felipe Meireles <139704068+FelipeMeireles1@users.noreply.github.com>
2023-07-16 14:28:14 -0300

    ex014 Conversor de temperatura.py

--- ex014 Conversor de temperatura.py
+++ ex014 Conversor de temperatura.py
@@ -1,4 +1,3 @@
 temperatura = float(input("Digite a temperatura em C°:"))
 print("{}C° convertidos para F° é {}F°".format(temperatura, temperatura*1.8+32))
-print("")
-print("")
+print("{}C° convertidos para K é {}K".format(temperatura, temperatura+273))
