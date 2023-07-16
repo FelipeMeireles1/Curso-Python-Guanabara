9c1559eccf3a9c592f79fcfd41c030b2a7235841
Felipe Meireles <139704068+FelipeMeireles1@users.noreply.github.com>
2023-07-16 14:25:03 -0300

    ex010 coversor de moedas.py
    Adicionado libra esterlina

--- ex010 coversor de moedas.py
+++ ex010 coversor de moedas.py
@@ -1,5 +1,7 @@
 real = float(input("Digite o valor em R$:"))
 dolar = real * 4.80
 euro = real * 5.38
+libra = real * 6.29
 print("Com R${:.0f} você pode comprar U${}".format(real, dolar))
 print("Com R${:.0f} você pode comprar {} Euros".format(real, euro))
+print("Com R${:.0f} você pode comprar {} Libras".format(real, libra))
